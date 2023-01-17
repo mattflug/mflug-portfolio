1cc4f0239721b0402ebeecae7d27ada3cb05bb61
Ghost <mbflug@gmail.com>
2023-01-16 17:44:20 -0700

    added grouping for cont1 & 2

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -3,16 +3,21 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Content1 from "../components/Content1";
-import Content1 from "../components/Content2";
+import Content2 from "../components/Content2";
 
 export default function Portfolio() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="sm">
-        <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} />
-        <Content1 />
-        <Content2 />
+      <Container maxWidth="lg">
+        <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} >
+        <div className="abox">
+          <Content1 />
+        </div>
+        <div className="bbox">
+          <Content2 />
+          </div>
+          </Box>
       </Container>
     </React.Fragment>
   );
