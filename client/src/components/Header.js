2cbb29632f33396c2e34639c85fdbc14df188aa6
Ghost <mbflug@gmail.com>
2023-01-18 11:13:12 -0700

    fixing linking

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,6 +11,7 @@ import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 
+
 const pages = ["About", "Portfolio", "Contact Me", "Resume"];
 
 function Header() {
