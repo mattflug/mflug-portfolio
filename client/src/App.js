497517846b90949fbba389dc6ffb6c9650f9146e
Ghost <mbflug@gmail.com>
2023-01-18 17:03:29 -0700

    adding routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,24 @@ import "./App.css";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
-import { BrowserRouter as Route } from "react-router-dom";
-import Portfolio from "./pages/Portfolio";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
-      <Header />
-      <Route path="/about" element={<About />} />
-      <Route path="/" element={<Projects />} />
-      <Route path="/contact" element={<Contact />} />
-      <Portfolio />
-      <Projects />
-      <Footer />
+      <Router>
+        <Header />
+        <div className="app">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </>
   );
 }
