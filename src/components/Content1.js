fa53cc7357fe2261d2e562d2815d3993b70a3fbd
Ghost <mbflug@gmail.com>
2023-01-31 10:43:02 -0700

    added breakpoints to height

--- src/components/Content1.js
+++ src/components/Content1.js
@@ -4,10 +4,10 @@ import Box from "@mui/material/Box";
 export default function Content1() {
   return (
     <Box
-      margin= {1}
+      margin={1}
       sx={{
         width: { xs: 500, sm: 250, md: 300, lg: 400, xl: 500 },
-        height: 250,
+        height: { xs: 500, sm: 250, md: 300, lg: 400, xl: 500 },
         backgroundColor: "primary.dark",
         "&:hover": {
           backgroundColor: "primary.main",
