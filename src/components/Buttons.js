4b5d7c626c880dc09ea38d9f77c75de8d45b6de3
Ghost <mbflug@gmail.com>
2023-02-01 09:55:39 -0700

    added pics

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -3,23 +3,21 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import ButtonBase from "@mui/material/ButtonBase";
 import Typography from "@mui/material/Typography";
-let bookies = require('../assets/bookies.jpg');
-let registry = require("../assets/the-registry.jpg");
-let rtc = require("../assets/rtc.jpg");
+
 
 const images = [
   {
-    url: { registry },
+    url: "https://www.extraspace.com/blog/wp-content/uploads/2019/02/wedding-registry-ideas.jpg",
     title: "registry",
     width: "40%",
   },
   {
-    url: { bookies },
+    url: "https://kottke.org/plus/misc/images/best-books-2016.jpg",
     title: "bookies",
     width: "30%",
   },
   {
-    url: { rtc },
+    url: "https://cdn.britannica.com/91/234691-159-C3D7476D/What-Are-Commodities-composite-image.jpg",
     title: "rtc",
     width: "30%",
   },
@@ -106,7 +104,7 @@ export default function Buttons() {
           <ImageBackdrop className="MuiImageBackdrop-root" />
           <Image>
             <Typography
-              component="span"
+              component=""
               variant="subtitle1"
               color="inherit"
               sx={{
