18a42a648e03785311aab3e5688d7aefb078805e
Ghost <mbflug@gmail.com>
2023-02-03 16:17:14 -0700

    adding components for about

---
+++ src/components/Bio.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+
+export default function Bio() {
+  return (
+    <Box
+      margin={1}
+      sx={{
+        width: { xs: 500, sm: 400, md: 600, lg: 800, xl: 1000 },
+        height: { xs: 800, sm: 500, md: 500, lg: 500, xl: 500 },
+        backgroundColor: "primary.dark",
+        "&:hover": {
+          backgroundColor: "primary.main",
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}
+    >
+      Bio
+    </Box>
+  );
+}
