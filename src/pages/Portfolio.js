f6bc357ca6b60b12c878a3c7478f121b5d4c6c61
Ghost <mbflug@gmail.com>
2023-02-03 15:13:36 -0700

    got content 2 to work

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,6 +3,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Content1 from "../components/Content1";
+import Content2 from "../components/Content2";
 
 import "./styling/portfolio.css";
 import Buttons from "../components/Buttons";
@@ -27,14 +28,9 @@ export default function Portfolio() {
             alignItems: { xs: "center" },
           }}
         >
-          <Content1 className="cont1">
-            
-          </Content1>
+          <Content1 className="cont1" />
 
-          <Content1 className="cont1">
-            <h1>hi</h1>
-            <h2>hellooooo</h2>
-          </Content1>
+          <Content2 className="cont2" />
         </Box>
         <Buttons className="projects" />
       </Container>
