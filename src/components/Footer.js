6aa6506e8d7a091f7d5315ba53528569b56fed13
Ghost <mbflug@gmail.com>
2023-02-08 09:56:21 -0700

    fixed icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import RestoreIcon from "@mui/icons-material/Restore";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Twitter from "@mui/icons-material/Twitter";
+import { LinkedIn } from "@mui/icons-material";
 
 export default function Footer() {
   const [value, setValue] = React.useState(0);
@@ -19,21 +19,21 @@ export default function Footer() {
         }}
       >
         <a href="https://github.com/mattflug" target="_blank" rel="noreferrer">
-          <BottomNavigationAction label="Github" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Github" icon={<GitHubIcon />} />
         </a>
         <a
           href="https://www.linkedin.com/in/matthew-flug1/"
           target="_blank"
           rel="noreferrer"
         >
-          <BottomNavigationAction label="LinkedIn" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="LinkedIn" icon={<LinkedIn />} />
         </a>
         <a
           href="https://twitter.com/matthewflug"
           target="_blank"
           rel="noreferrer"
         >
-          <BottomNavigationAction label="Twitter" icon={<LocationOnIcon />} />
+          <BottomNavigationAction label="Twitter" icon={<Twitter />} />
         </a>
       </BottomNavigation>
     </Box>
